4cbbb73fa9ddec67a9a34f34dea25ee571200833
Topo Lefika Rapula <cse21-077@thuto.bac.ac.bw>
2025-03-15 21:41:30 +0200

    Update next.config.ts
    fix swc error

--- next.config.ts
+++ next.config.ts
@@ -8,7 +8,7 @@ const withPWA = require("next-pwa")({
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: false, // ⬅️ Disable SWC minifier (fallback to Terser)
 }
 
 module.exports = withPWA(nextConfig)
-
